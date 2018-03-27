7fa7d47194b64444d5d199270eab23c1ec6e82a5
Arpit Aggarwal <ar13pit@gmail.com>
2018-03-27 15:27:01 +0200

    pylint check complete. Add documentation. Write module as a class

--- tools/prepare.py
+++ tools/prepare.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+"""This module has been written to prepare the files and directory structure
+required by Kaldi-ASR to develop Acoustic Models."""
 
 import os
-import shutil
-import sys
-import time
 
 def text(root_dir):
+    """Prepares the file 'text' """
     if root_dir[-1] != '/':
         root_dir += '/'
 
@@ -25,28 +25,30 @@ def text(root_dir):
     tfname.close()
 
 def wavscp(root_dir):
+    """Prepares the file 'wav.scp' """
     if root_dir[-1] != '/':
         root_dir += '/'
     wav_dir = root_dir + 'wav/'
     wav_contents = os.listdir(wav_dir)
     wav_contents.sort()
     out = ''
-    for f in wav_contents:
-        out += f.split('.')[0] + '\t' + wav_dir + f + '\n'
+    for wav_file in wav_contents:
+        out += wav_file.split('.')[0] + '\t' + wav_dir + wav_file + '\n'
     out = out[:-1]
     wfname = open(root_dir + 'wav.scp', 'w')
     wfname.write(out)
     wfname.close()
 
 def utt2spk(root_dir):
+    """Prepares the file 'utt2spk' """
     if root_dir[-1] != '/':
         root_dir += '/'
     text_dir = root_dir + 'txt/'
     text_contents = os.listdir(text_dir)
     text_contents.sort()
     out = ''
-    for f in text_contents:
-        fname = f.split('.')[0]
+    for text_file in text_contents:
+        fname = text_file.split('.')[0]
         out += fname + '\t' + fname[:-4] + '\n'
     out = out[:-1]
     uttfname = open(root_dir + 'utt2spk', 'w')
@@ -54,6 +56,7 @@ def utt2spk(root_dir):
     uttfname.close()
 
 def spk2utt(root_dir):
+    """Prepares the file 'spk2utt' """
     if root_dir[-1] != '/':
         root_dir += '/'
     text_dir = root_dir + 'txt/'
@@ -61,8 +64,8 @@ def spk2utt(root_dir):
     text_contents.sort()
     spk_begin = (text_contents[0].split('.'))[0][:-4]
     out = spk_begin
-    for f in text_contents:
-        fname = f.split('.')[0]
+    for text_file in text_contents:
+        fname = text_file.split('.')[0]
         if fname[:-4] == spk_begin:
             out += ' ' + fname
         else:
@@ -71,67 +74,3 @@ def spk2utt(root_dir):
     spkfname = open(root_dir + 'spk2utt', 'w')
     spkfname.write(out)
     spkfname.close()
-
-
-path = input("Enter path to the transcribed data: ")
-if not os.path.exists(path):
-    print("Transcribed data directory path does not exist")
-    sys.exit(-1)
-
-if path[-1] != '/':
-    path += '/'
-
-out = input("Enter path to output directory: ")
-if not os.path.exists(out):
-    print("Output data directory path does not exist")
-    sys.exit(-1)
-
-if out[-1] != '/':
-    out += '/'
-
-date = time.strftime("%Y-%m-%d-%H:%M")
-out = out + "KalDAT_" + date + '/'
-os.mkdir(out)
-
-DnM = out + 'DnM/'
-os.mkdir(DnM)
-DnM_text = DnM + 'txt/'
-DnM_wav = DnM + 'wav/'
-os.mkdir(DnM_text)
-os.mkdir(DnM_wav)
-
-Comm = out + 'Comm/'
-os.mkdir(Comm)
-Comm_text = Comm +'txt/'
-Comm_wav = Comm + 'wav/'
-os.mkdir(Comm_text)
-os.mkdir(Comm_wav)
-
-path_contents = os.listdir(path)
-
-for spk in path_contents:
-    spk_path = path + spk + '/'
-    spk_contents = os.listdir(spk_path)
-
-    if (spk[8] == 'A') or (spk[8] == 'F'):
-        for f in spk_contents:
-            if f.split('.')[-1] == 'wav':
-                shutil.copy(spk_path + f, DnM_wav)
-            if f.split('.')[-1] == 'txt':
-                shutil.copy(spk_path + f, DnM_text)
-
-    if (spk[8] == 'B') or (spk[8] == 'C') or (spk[8] == 'E'):
-        for f in spk_contents:
-            if f.split('.')[-1] == 'wav':
-                shutil.copy(spk_path + f, Comm_wav)
-            if f.split('.')[-1] == 'txt':
-                shutil.copy(spk_path + f, Comm_text)
-
-text(DnM)
-text(Comm)
-wavscp(DnM)
-wavscp(Comm)
-utt2spk(DnM)
-utt2spk(Comm)
-spk2utt(DnM)
-spk2utt(Comm)
