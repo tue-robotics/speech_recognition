4cd819972386c871c23cc6d7746644c5569f3549
Arpit Aggarwal <ar13pit@gmail.com>
2018-04-07 21:11:08 +0200

    added classes Flags and Index. Linting almost complete

--- tools/prepare.py
+++ tools/prepare.py
@@ -24,6 +24,27 @@ import sys
 import csv
 import shutil
 
+class Flags:
+    """Class Flags takes no input arguments and initializes all flags to False"""
+    def __init__(self):
+        self.csv_check = False
+        self.csv_header = False
+        self.wav_path = False
+        self.transcription = False
+        self.gender = False
+        self.utterance_id = False
+        self.speaker_id = False
+
+class Index:
+    """Class Index has been defined to maintain the indices of the CSV file."""
+    def __init__(self):
+        self.wav_path = None
+        self.transcription = None
+        self.gender = None
+        self.speaker_id = None
+        self.utterance_id = None
+
+
 class DataPreparation:
     """Class DataPreparation takes 2 input arguments
 
@@ -33,55 +54,58 @@ class DataPreparation:
         param3 (str): Type of Dataset
 
     """
-    def __init__(self, csv_path, srs_path, dataSet):
+    def __init__(self, csv_path, srs_path, dataset):
         """ DataPreparation class constructor """
 
         file_extension = csv_path.split('.')[-1]
         if file_extension != 'csv':
             sys.exit("File type not '.csv'")
 
-        if !os.path.exists(csv_path):
+        if not os.path.exists(csv_path):
             sys.exit("Invalid CSV path")
 
-        self.CSV_PATH = csv_path
-        self.CSV_DATA_ROOT = os.path.dirname(csv_path)
+        self.csv_path = csv_path
+        self.csv_data_root = os.path.dirname(csv_path)
 
-        self.csvCheck
+        self.flag = Flags()
+        self.index = Index()
+
+        self.csv_check()
 
         # Return the exact error
-        if !self.FLAG.CSV_CHECK:
+        if not self.flag.csv_check:
             sys.exit("CSV check failed")
 
         if srs_path[-1] == '/':
             srs_path = srs_path[:-1]
 
-        if !os.path.exists(srs_path):
+        if not os.path.exists(srs_path):
             sys.exit("Invalid path to Speech Recognition System root")
 
         # This section maybe buggy
-        self.SRS_PATH = srs_path
-        self.SRS_PATH_DATA = self.SRS_PATH + "/data"
-        self.SRS_PATH_DATA_DATASET = self.SRS_PATH_DATA + "/" + dataSet
+        self.srs_path = srs_path
+        self.srs_path_data = self.srs_path + "/data"
+        self.srs_path_data_dataset = self.srs_path_data + "/" + dataset
 
-        if os.path.exists(self.SRS_PATH_DATA):
-            data_dir_contents = os.listdir(self.SRS_PATH_DATA)
+        if os.path.exists(self.srs_path_data):
+            data_dir_contents = os.listdir(self.srs_path_data)
             if data_dir_contents == []:
-                os.mkdir(self.SRS_PATH_DATA_DATASET)
+                os.mkdir(self.srs_path_data_dataset)
             else:
                 data_dir_contents.remove('test')
                 for directory in data_dir_contents:
-                    shutil.rmtree(self.SRS_PATH_DATA + "/" + directory)
-                os.mkdir(self.SRS_PATH_DATA_DATASET)
+                    shutil.rmtree(self.srs_path_data + "/" + directory)
+                os.mkdir(self.srs_path_data_dataset)
         else:
-            os.mkdir(self.SRS_PATH_DATA)
-            os.mkdir(self.SRS_PATH_DATA_DATASET)
+            os.mkdir(self.srs_path_data)
+            os.mkdir(self.srs_path_data_dataset)
 
-        self.text
-        self.spk2gender
-        self.wavscp
-        self.spk2utt
+        self.text()
+        self.spk2gender()
+        self.wavscp()
+        self.spk2utt()
 
-    def csvCheck(self):
+    def csv_check(self):
         """csvCheck performs the preliminary checks before the construction of
         the required data files.
 
@@ -91,35 +115,35 @@ class DataPreparation:
             3. Check whether the Header contains fields and store their indices:
                 a. SPEAKER_ID
                 b. WAV_PATH         (relative to the CSV file)
-                c. TRANSCRIPTION
+                c. transcription
                 d. GENDER
                 e. UTTERANCE_ID
             4. Read every row of the CSV file and check if all the wav paths exist
             5. Check whether number of wav files and transcriptions are equal
         """
-        with open(self.CSV_PATH, 'r') as csv_file:
+        with open(self.csv_path, 'r') as csv_file:
             header = csv_file.readline()
             sniffer = csv.Sniffer()
             dialect = sniffer.sniff(header)
-            self.CSV_DELIMITER = dialect.delimiter
+            self.csv_delimiter = dialect.delimiter
 
-        with open(self.CSV_PATH, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = self.CSV_DELIMITER)
-            self.CSV_HEADER = next(csv_reader)
+        with open(self.csv_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=self.csv_delimiter)
+            csv_header = next(csv_reader)
 
             try:
-                self.INDEX.WAV_PATH = self.CSV_HEADER.index("WAV_PATH")
-                self.INDEX.TRANSCRIPTION = self.CSV_HEADER.index("TRANSCRIPTION")
-                self.INDEX.GENDER = self.CSV_HEADER.index("GENDER")
-                self.INDEX.SPEAKER_ID = self.CSV_HEADER.index("SPEAKER_ID")
-                self.INDEX.UTTERANCE_ID = self.CSV_HEADER.index("UTTERANCE_ID")
+                self.index.wav_path = csv_header.index("WAV_PATH")
+                self.index.transcription = csv_header.index("TRANSCRIPTION")
+                self.index.gender = csv_header.index("GENDER")
+                self.index.speaker_id = csv_header.index("SPEAKER_ID")
+                self.index.utterance_id = csv_header.index("UTTERANCE_ID")
 
             except:
-                self.FLAG.CSV_HEADER = False
-                self.FLAG.CSV_CHECK = False
+                self.flag.csv_header = False
+                self.flag.csv_check = False
                 return
             else:
-                self.FLAG.CSV_HEADER = True
+                self.flag.csv_header = True
 
             wav_files = 0
             transcriptions = 0
@@ -128,63 +152,59 @@ class DataPreparation:
             utterance_count = 0
 
             for row in csv_reader:
-                wav_rel_path = row[self.INDEX.WAV_PATH]
-                wav_path = self.CSV_DATA_ROOT + '/' + wav_rel_path
-                if !os.path.exists(wav_path):
-                    self.FLAG.WAV_PATH = False
-                    self.FLAG.CSV_CHECK = False
+                wav_rel_path = row[self.index.wav_path]
+                wav_path = self.csv_data_root + '/' + wav_rel_path
+                if not os.path.exists(wav_path):
+                    self.flag.wav_path = False
+                    self.flag.csv_check = False
                     return
                 wav_files += 1
 
-                if row[self.INDEX.TRANSCRIPTION] != '':
+                if row[self.index.transcription] != '':
                     transcriptions += 1
 
-                if row[self.INDEX.GENDER] != '':
+                if row[self.index.gender] != '':
                     gender_count += 1
 
-                if row[self.INDEX.SPEAKER_ID] != '':
+                if row[self.index.speaker_id] != '':
                     speaker_count += 1
 
-                if row[self.INDEX.UTTERANCE_ID] != '':
+                if row[self.index.utterance_id] != '':
                     utterance_count += 1
 
-            self.FLAG.WAV_PATH = True
+            self.flag.wav_path = True
 
-            self.WAV_FILES = wav_files
-            self.TRANSCRIPTIONS = transcriptions
-            self.GENDER_COUNT = gender_count
-            self.SPEAKER_COUNT = speaker_count
-            self.UTTERANCE_COUNT = utterance_count
+            self.total_files = wav_files
 
-            if self.TRANSCRIPTIONS != self.WAV_FILES:
-                self.FLAG.TRANSCRIPTION = False
-                self.FLAG.CSV_CHECK = False
+            if transcriptions != self.total_files:
+                self.flag.transcription = False
+                self.flag.csv_check = False
                 return
             else:
-                self.FLAG.TRANSCRIPTION = True
+                self.flag.transcription = True
 
-            if self.GENDER_COUNT != self.WAV_FILES:
-                self.FLAG.GENDER = False
-                self.FLAG.CSV_CHECK = False
+            if gender_count != self.total_files:
+                self.flag.gender = False
+                self.flag.csv_check = False
                 return
             else:
-                self.FLAG.GENDER = True
+                self.flag.gender = True
 
-            if self.SPEAKER_COUNT != self.WAV_FILES:
-                self.FLAG.SPEAKER_ID = False
-                self.FLAG.CSV_CHECK = False
+            if speaker_count != self.total_files:
+                self.flag.speaker_id = False
+                self.flag.csv_check = False
                 return
             else:
-                self.FLAG.SPEAKER_ID = True
+                self.flag.speaker_id = True
 
-            if self.UTTERANCE_COUNT != self.WAV_FILES:
-                self.FLAG.UTTERANCE_ID = False
-                self.FLAG.CSV_CHECK = False
+            if utterance_count != self.total_files:
+                self.flag.utterance_id = False
+                self.flag.csv_check = False
                 return
             else:
-                self.FLAG.UTTERANCE_ID = True
+                self.flag.utterance_id = True
 
-            self.FLAG.CSV_CHECK = True
+            self.flag.csv_check = True
 
     def text(self):
         """text prepares the file 'text' in the DATASET directory.
@@ -199,19 +219,19 @@ class DataPreparation:
         4. Write an output file where each line has the structure:
             <FILE_ID><Tab_space><TRANSCRIPTION>
         """
-        with open(self.CSV_PATH, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = self.CSV_DELIMITER)
+        with open(self.csv_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=self.csv_delimiter)
             row = next(csv_reader)
 
-            iterations = self.WAV_FILES
+            iterations = self.total_files
             out = ''
 
             while iterations:
                 row = next(csv_reader)
 
-                sid = row[self.INDEX.SPEAKER_ID]
-                uid = row[self.INDEX.UTTERANCE_ID]
-                transcription = row[self.INDEX.TRANSCRIPTION]
+                sid = row[self.index.speaker_id]
+                uid = row[self.index.utterance_id]
+                transcription = row[self.index.transcription]
 
                 fid = sid + 'U' + str(uid).zfill(5)
 
@@ -220,7 +240,7 @@ class DataPreparation:
 
         # The output file must not end with a newline
         out = out[:-1]
-        with open(self.SRS_PATH_DATA_DATASET + '/text', 'w') as out_file:
+        with open(self.srs_path_data_dataset + '/text', 'w') as out_file:
             out_file.write(out)
 
     def spk2gender(self):
@@ -233,43 +253,43 @@ class DataPreparation:
         4. Write an output file where each line has the structure:
             <SPEAKER_ID><Tab_space><GENDER>
         """
-        with open(self.CSV_PATH, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = self.CSV_DELIMITER)
+        with open(self.csv_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=self.csv_delimiter)
             row = next(csv_reader)
 
-            iterations = self.WAV_FILES
+            iterations = self.total_files
             out = ''
 
             # First speaker is not a new speaker
             row = next(csv_reader)
-            sid = row[self.INDEX.SPEAKER_ID]
-            gender = row[self.INDEX.GENDER]
+            sid = row[self.index.speaker_id]
+            gender = row[self.index.gender]
             out += sid + '\t' + gender + '\n'
             iterations -= 1
-            newSpeaker = False
+            new_speaker = False
 
             while iterations:
-                if newSpeaker:
+                if new_speaker:
 
-                    sid = row[self.INDEX.SPEAKER_ID]
-                    gender = row[self.INDEX.GENDER]
+                    sid = row[self.index.speaker_id]
+                    gender = row[self.index.gender]
 
                     out += sid + '\t' + gender + '\n'
                     iterations -= 1
-                    newSpeaker = False
+                    new_speaker = False
 
                 else:
                     row = next(csv_reader)
 
-                    if row[self.INDEX.SPEAKER_ID] == sid:
+                    if row[self.index.speaker_id] == sid:
                         iterations -= 1
                         continue
                     else:
-                        newSpeaker = True
+                        new_speaker = True
 
         # The output file must not end with a newline
         out = out[:-1]
-        with open(self.SRS_PATH_DATA_DATASET + '/spk2gender', 'w') as out_file:
+        with open(self.srs_path_data_dataset + '/spk2gender', 'w') as out_file:
             out_file.write(out)
 
     def wavscp(self):
@@ -286,29 +306,29 @@ class DataPreparation:
         5. Write an output file where each line has the structure:
             <FILE_ID><Tab_space><FILE_PATH>
         """
-        with open(self.CSV_PATH, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = self.CSV_DELIMITER)
+        with open(self.csv_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=self.csv_delimiter)
             row = next(csv_reader)
 
-            iterations = self.WAV_FILES
+            iterations = self.total_files
             out = ''
 
             while iterations:
                 row = next(csv_reader)
 
-                sid = row[self.INDEX.SPEAKER_ID]
-                uid = row[self.INDEX.UTTERANCE_ID]
-                wav_rel_path = row[self.INDEX.WAV_PATH]
+                sid = row[self.index.speaker_id]
+                uid = row[self.index.utterance_id]
+                wav_rel_path = row[self.index.wav_path]
 
                 fid = sid + 'U' + str(uid).zfill(5)
-                fpath = self.CSV_DATA_ROOT + '/' + wav_rel_path
+                fpath = self.csv_data_root + '/' + wav_rel_path
 
                 out += fid + '\t' + fpath + '\n'
                 iterations -= 1
 
         # The output file must not end with a newline
         out = out[:-1]
-        with open(self.SRS_PATH_DATA_DATASET + '/wav.scp', 'w') as out_file:
+        with open(self.srs_path_data_dataset + '/wav.scp', 'w') as out_file:
             out_file.write(out)
 
     def utt2spk(self):
@@ -323,18 +343,18 @@ class DataPreparation:
         4. Write an output file where each line has the structure:
             <FILE_ID><Tab_space><SPEAKER_ID>
         """
-        with open(self.CSV_PATH, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = self.CSV_DELIMITER)
+        with open(self.csv_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=self.csv_delimiter)
             row = next(csv_reader)
 
-            iterations = self.WAV_FILES
+            iterations = self.total_files
             out = ''
 
             while iterations:
                 row = next(csv_reader)
 
-                sid = row[self.INDEX.SPEAKER_ID]
-                uid = row[self.INDEX.UTTERANCE_ID]
+                sid = row[self.index.speaker_id]
+                uid = row[self.index.utterance_id]
 
                 fid = sid + 'U' + str(uid).zfill(5)
 
@@ -343,7 +363,7 @@ class DataPreparation:
 
         # The output file must not end with a newline
         out = out[:-1]
-        with open(self.SRS_PATH_DATA_DATASET + '/utt2spk', 'w') as out_file:
+        with open(self.srs_path_data_dataset + '/utt2spk', 'w') as out_file:
             out_file.write(out)
 
     def spk2utt(self):
@@ -357,11 +377,11 @@ class DataPreparation:
         3. Write an output file where each line has the structure:
            <SPEAKER_ID> <FILE_ID_1> <FILE_ID_2> ... <FILE_ID_END>
         """
-        if not os.path.exists(self.SRS_PATH_DATA_DATASET + "/utt2spk"):
-            self.utt2spk
+        if not os.path.exists(self.srs_path_data_dataset + "/utt2spk"):
+            self.utt2spk()
 
-        with open(self.SRS_PATH_DATA_DATASET + '/utt2spk', 'r') as utt2spk:
-            iterations = self.WAV_FILES
+        with open(self.srs_path_data_dataset + '/utt2spk', 'r') as utt2spk:
+            iterations = self.total_files
             out = ''
 
             # First speaker is not a new speaker
@@ -370,12 +390,12 @@ class DataPreparation:
             sid = row[1]
             out += sid + ' ' + fid
             iterations -= 1
-            newSpeaker = False
+            new_speaker = False
 
             while iterations:
-                if newSpeaker:
+                if new_speaker:
                     out += '\n' + sid + ' ' + fid
-                    newSpeaker = False
+                    new_speaker = False
                     iterations -= 1
 
                 else:
@@ -387,11 +407,11 @@ class DataPreparation:
                         iterations -= 1
 
                     else:
-                        newSpeaker = True
+                        new_speaker = True
 
         # The output file must not end with a newline
         # out = out[:-1]
-        with open(self.SRS_PATH_DATA_DATASET + '/spk2utt', 'w') as out_file:
+        with open(self.srs_path_data_dataset + '/spk2utt', 'w') as out_file:
             out_file.write(out)
 
 def main():
@@ -408,9 +428,9 @@ def main():
     else:
         csv_path = sys.argv[1]
         srs_path = sys.argv[2]
-        dataSet = sys.argv[3]
+        dataset = sys.argv[3]
 
-        DataPreparation(csv_path, srs_path, dataSet)
+        DataPreparation(csv_path, srs_path, dataset)
 
  # Add the main() subroutine
 if __name__ == '__main__':
