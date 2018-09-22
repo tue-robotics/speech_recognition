bcdd702428a0651d6e9041621d6460df6fb78a4d
Arpit Aggarwal <ar13pit@gmail.com>
2018-09-22 11:47:24 +0200

    updated unit tests script

--- ci/travis-unittests.bash
+++ ci/travis-unittests.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\e[33m\e[1mExecuting script for unit tests \e[0m"
+echo -e "\e[33m\e[1mExecuting Unit Tests \e[0m"
 
-docker exec arpit-env ./_git/unittest_1.bash
+docker exec hero-env ./_git/tests/digits.bash
 
