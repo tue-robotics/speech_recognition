80b9d59a578a865e50909467b6af29231c9a5db5
Arpit Aggarwal <ar13pit@gmail.com>
2018-09-22 14:04:05 +0200

    added ENV variables check

--- tests/digits.bash
+++ tests/digits.bash
@@ -5,16 +5,21 @@
 # Arpit Aggarwal
 # 2018 TechUnited @Home, TU Eindhoven
 
-kaldi_test()
-{
-	log "Testing Kaldi installation"
-	cd $ASR_HOME/egs
-	tar -xvzf digits.tar.gz
-	cd digits
-	sed -i "1iexport KALDI_ROOT=$KALDI" path.sh
-	mkdir steps utils
-	ln -s $KALDI/egs/wsj/s5/steps steps
-	ln -s $KALDI/egs/wsj/s5/utils utils
+if [ [ -z "$KALDI_ROOT" ] || [ -z "$ASR_HOME" ] || [ -z "$ASR_LOG" ] ]
+then
+	echo -e "\e[35m\e[1m Environment variables do not exist \e[0m"
+    exit 1
+fi
 
-}
+echo -e "\e[33m\e[1m Testing Kaldi installation \e[0m"
+
+cd $ASR_HOME/egs
+tar -xvzf digits.tar.gz
+cd digits
+sed -i "1iexport KALDI_ROOT=$KALDI_ROOT" path.sh
+
+mkdir steps utils
+ln -s $KALDI/egs/wsj/s5/steps steps
+ln -s $KALDI/egs/wsj/s5/utils utils
+
 
