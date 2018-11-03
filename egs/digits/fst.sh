c92df5ccfb89ce9582a98a712e391c30c4d5d296
Arpit Aggarwal <ar13pit@gmail.com>
2018-11-03 11:45:47 +0100

    corrected directory paths

--- egs/digits/fst.sh
+++ egs/digits/fst.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-data_dir=/home/bitmesra/repositories/kaldi/egs/digits/data
-lang_dir=/home/bitmesra/repositories/kaldi/egs/digits/data/lang
-tmp_dir=/home/bitmesra/repositories/kaldi/egs/digits/data/local/tmp
+#! /usr/bin/env bash
+#
+# Script to prepare the Language Model FST
 
+# Set directory paths
+eg_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+data_dir=$eg_dir/data
+lang_dir=$data_dir/lang
+tmp_dir=$data_dir/local/tmp
 
+# Create directory to store temp files
 mkdir -p $tmp_dir
 
 utils/prepare_lang.sh $data_dir/local/dict '!SIL' $data_dir/local/lang $data_dir/lang || exit 1;
@@ -18,3 +23,4 @@ fstcompile --isymbols=$lang_dir/words.txt --osymbols=$lang_dir/words.txt \
    --keep_isymbols=false --keep_osymbols=false $tmp_dir/G.txt > $lang_dir/G.fst
 
 utils/validate_lang.pl $lang_dir # Note; this actually does report errors,
+
