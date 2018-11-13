9ac8703ce3b4aeb009bdb53311704b4094fdcd4d
Arpit Aggarwal <ar13pit@gmail.com>
2018-11-13 20:54:40 +0100

    merged run.bash with online tutorial script

--- egs/digits/run.bash
+++ egs/digits/run.bash
@@ -2,29 +2,61 @@
 
 train_cmd="utils/run.pl"
 decode_cmd="utils/run.pl"
+nj=1
 
-# Feature extraction
-for x in train test
-do
-    steps/make_mfcc.sh --nj 1 data/$x exp/make_mfcc/$x mfcc || exit 1
-    steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x mfcc || exit 1
-    utils/fix_data_dir.sh data/$x
-done
+echo
+echo "===== PREPARING ACOUSTIC DATA ====="
+echo
+# Making spk2utt files
+utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
 
-echo "Features Extracted Successfully."
+echo
+echo "===== FEATURES EXTRACTION ====="
+echo
+# Making feats.scp files
+mfccdir=mfcc
+# Uncomment and modify arguments in scripts below if you have any problems with data sorting
+# utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
+# utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train exp/make_mfcc/train $mfccdir
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/test exp/make_mfcc/test $mfccdir
 
-# Mono training
-steps/train_mono.sh --nj 1 --cmd "$train_cmd" --totgauss 1600 data/train data/lang exp/mono0a || exit 1
+# Making cmvn.scp files
+steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
+steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
 
-echo "Mono Training Complete."
+echo
+echo "===== MONO TRAINING ====="
+echo
+# Test without gaussians as well
+steps/train_mono.sh --nj $nj --cmd "$train_cmd" --totgauss 1600 data/train data/lang exp/mono  || exit 1
 
-# Graph compilation
-utils/mkgraph.sh --mono data/lang exp/mono0a exp/mono0a/graph_tgpr || exit 1
+echo
+echo "===== MONO DECODING ====="
+echo
+utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
 
-# Decoding
-#steps/decode.sh --nj 1 --cmd "$decode_cmd" \
- #   exp/mono0a/graph_tgpr data/test_digits exp/mono0a/decode_test_digits
-steps/decode.sh --config conf/decode.config --nj 1 --cmd "$decode_cmd" exp/mono0a/graph_tgpr data/test exp/mono0a/decode_test_digits || exit 1
+echo
+echo "===== MONO ALIGNMENT ====="
+echo
+steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali || exit 1
+
+echo
+echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo
+steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1
+
+echo
+echo "===== TRI1 (first triphone pass) DECODING ====="
+echo
+utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
+
+echo
+echo "===== run.sh script is finished ====="
+echo
 
 for x in exp/*/decode*
 do
