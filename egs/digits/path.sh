99473819f64ff3624feb52bba9207f1cdbc981dc
Arpit Aggarwal <ar13pit@gmail.com>
2018-11-13 20:29:59 +0100

    added src/lmbin to path to use arpa2fst

--- egs/digits/path.sh
+++ egs/digits/path.sh
@@ -4,7 +4,7 @@
 
 # Add Kaldi libraries to path
 # @TODO: Make these a part of setup.bash in KALDI repo
-export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/onlinebin:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/lmbin/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/onlinebin:$PWD:$PATH
 
 # Export audio data directory
 digits_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
