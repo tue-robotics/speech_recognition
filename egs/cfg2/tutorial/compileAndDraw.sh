08e94e8678a50a8df3d9d89ee70a00ec94740dbe
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-02 20:38:16 +0100

    cleaned up

--- egs/cfg2/tutorial/compileAndDraw.sh
+++ egs/cfg2/tutorial/compileAndDraw.sh
@@ -1,41 +1,46 @@
-#!bash
-#Create the example using this script
-if [[ $# -eq 0 ]] ; then
-	echo 'One argument must be given of the form \"name.fst.txt\" or \"name.fsa.txt\"';
-	exit 1;
+#! /usr/bin/env bash
+# Create the example using this script
+if [[ $# -eq 0 ]]
+then
+    echo 'One argument must be given of the form \"name.fst.txt\" or \"name.fsa.txt\"'
+    exit 1
 fi
 
-IFS="."; #Make . the split character
-inputFilenameParts=($1);
-IFS=" "; #Put it back to default space
+# Make . the split character
+IFS="."
+inputFilenameParts=($1)
+# Put it back to default space
+IFS=" "
 
-name=${inputFilenameParts[0]};
-type=${inputFilenameParts[1]};
+name=${inputFilenameParts[0]}
+type=${inputFilenameParts[1]}
 
-echo 'Preparing: '$name;
+echo 'Preparing: '$name
 
-fsTextFile="${name}.${type}.txt";
-fsFile="$name.${type}";
-osymsFile="${name}.osyms";
-isymsFile="${name}.isyms";
-svgOutputFile="${name}.svg";
+fsTextFile="${name}.${type}.txt"
+fsFile="$name.${type}"
+osymsFile="${name}.osyms"
+isymsFile="${name}.isyms"
+svgOutputFile="${name}.svg"
 
-isymbols="--isymbols=${isymsFile}";
-osymbols="--osymbols=${osymsFile}";
+isymbols="--isymbols=${isymsFile}"
+osymbols="--osymbols=${osymsFile}"
 
 
-python makeSymbols.py $fsTextFile 2 > $isymsFile;
+python makeSymbols.py $fsTextFile 2 > $isymsFile
 
-if [ $type = "fst" ] ; then
-    python makeSymbols.py $fsTextFile 3 > $osymsFile;
-    fstcompile $isymbols $osymbols --keep_isymbols --keep_osymbols $fsTextFile $fsFile;
-    fstdraw --portrait $fsFile  | dot -Tsvg > $svgOutputFile;
-elif [ $type = "fsa" ] ; then
-    fstcompile --acceptor $isymbols --keep_isymbols $fsTextFile $fsFile;
-    fstdraw --portrait $fsFile | dot -Tsvg > $svgOutputFile;
+if [ $type = "fst" ]
+then
+    python makeSymbols.py $fsTextFile 3 > $osymsFile
+    fstcompile $isymbols $osymbols --keep_isymbols --keep_osymbols $fsTextFile $fsFile
+    fstdraw --portrait $fsFile  | dot -Tsvg > $svgOutputFile
+elif [ $type = "fsa" ]
+then
+    fstcompile --acceptor $isymbols --keep_isymbols $fsTextFile $fsFile
+    fstdraw --portrait $fsFile | dot -Tsvg > $svgOutputFile
 else
-    echo "Filetype: ${type} not recognitsed. Recognised types are fst=finite state trasducer and fsa=finite state acceptor";
-fi 
+    echo "Filetype: ${type} not recognitsed. Recognised types are fst=finite state trasducer and fsa=finite state acceptor"
+fi
 
 echo 'Done, outputted: ' $svgOutputFile
 
