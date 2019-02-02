8d1d2df233c7392ae48f7479c646b7b5195b0bf0
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-02 20:46:46 +0100

    added bin and build

--- egs/cfg2/tutorial/composeExample.sh
+++ egs/cfg2/tutorial/composeExample.sh
@@ -1,19 +1,24 @@
-#!bin/sh
+#! /usr/bin/env bash
 
 # Based on http://www.isle.illinois.edu/sst/courses/minicourses/2009/lecture6.pdf
+
+mkdir -p build bin
+BIN=bin
+BUILD=build
 
 bash compileAndDraw.sh sent.fsa
 bash compileAndDraw.sh dict.fst
 
-fstcompose --fst_compat_symbols=false sent.fsa dict.fst > strings.fst
-fstdraw --portrait  strings.fst | dot -Tsvg >  strings.svg
+fstcompose --fst_compat_symbols=false $BIN/sent.fsa $BIN/dict.fst > $BIN/strings.fst
+fstdraw --portrait  $BIN/strings.fst | dot -Tsvg >  $BIN/strings.svg
+
 echo 'Done composing: outputted strings.svg'
 echo 'Example sentences:'
 echo '------------------'
 
-for i in `seq 1 10`;
+for i in $(seq 1 10)
 do
-	fstrandgen --seed=$RANDOM strings.fst | fstproject --project_output |
-	fstprint --acceptor --isymbols=dict.syms |
-	awk '{printf("%s ",$3)}END{printf("\n")}'
+    fstrandgen --seed=$RANDOM $BUILD/strings.fst | fstproject --project_output |
+    fstprint --acceptor --isymbols=dict.syms |
+    awk '{printf("%s ",$3)}END{printf("\n")}'
 done
