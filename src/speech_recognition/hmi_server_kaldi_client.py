a79c2ae3bfc965378e7df90be85c46fc9622813c
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-07 15:48:59 +0100

    added comments and cleaned up

--- src/speech_recognition/hmi_server_kaldi_client.py
+++ src/speech_recognition/hmi_server_kaldi_client.py
@@ -1,27 +1,34 @@
-import rospy
-from hmi import AbstractHMIServer, HMIResult
+#! /usr/bin/env python
+#
+# ROS Node and HMI Client of Kaldi Gstreamer App
 
-# New imports
-from speech_recognition.kaldi_gstreamer_app import KaldiGstApp
 import sys
 import os
+
 # Gstreamer imports
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import GObject, Gst
+from speech_recognition.kaldi_gstreamer_app import KaldiGstApp
 
+# ROS imports
+import rospy
+from hmi import AbstractHMIServer, HMIResult
 
 class HMIServerKaldiClient(AbstractHMIServer):
-
+    """Subclass of AbstractHMIServer that creates a HMI client for
+    KaldiGstApp"""
     def __init__(self):
-        """
-        DragonflyHMIServer that exposes the HMI ROS Server and holds a socket client that talks to the dragonfly
-        speech recognition server.
-        """
-        super(HMIServerKaldiClient, self).__init__(rospy.get_name())
-        self.model_path = rospy.get_param("/kaldi_model_path")     # Assuming this param has been set somewhere
+        """Class constructor that initializes the parent AbstractHMIServer class
+        and gets parameters from ROS parameter server"""
+        AbstractHMIServer.__init__(rospy.get_name())
+
+        # Get the kaldi model path from ROS Parameter server
+        self.model_path = rospy.get_param("/kaldi_model_path")
 
     def _determine_answer(self, description, grammar, target, is_preempt_requested):
+        """Method override to start speech recognition upon receiving a query
+        from the HMI server"""
         # Todo: Take in grammar and target and compose the HCLG.fst (speech recognition graph) through a different
         #   class or whatever, before initialising the KaldiGstApp
 
@@ -38,3 +45,4 @@ class HMIServerKaldiClient(AbstractHMIServer):
             print(kaldi_app.sentence)
             return HMIResult(kaldi_app.sentence, "")
         return None
+
