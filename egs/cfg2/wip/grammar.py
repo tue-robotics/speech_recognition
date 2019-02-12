3c8bea4bf97f51b887757aa13248d24ad595e21e
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-12 21:48:00 +0100

    added word extraction from grammar

--- egs/cfg2/wip/grammar.py
+++ egs/cfg2/wip/grammar.py
@@ -1,7 +1,26 @@
 #! /usr/bin/env python
 import sys
-from grammar_parser import cfgparser
+from grammar_parser.cfgparser import CFGParser
 
-p = cfgparser.CFGParser.fromfile('grammar.fcfg')
-s = p.get_random_sentence('T')
-print(s)
+words = set()
+
+p = CFGParser.fromfile('current_grammar.fcfg')
+rules = p.rules
+
+for key, value in rules.iteritems():
+    # Get the list of options for the rule value
+    options = value.options
+    for option in options:
+        # Get the list of conjuncts for option 'option'
+        conjuncts = option.conjuncts
+        for conjunct in conjuncts:
+            # If conjunct is not a variable put its value in the set of words
+            if not conjunct.is_variable:
+                words.add(conjunct.name)
+
+
+print(words)
+
+
+#s = p.get_random_sentence('T')
+#print(s)
