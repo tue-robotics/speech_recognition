97ad3683b86d494cbca8cacb6e5dcc42fab01a5c
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-09 23:00:07 +0100

    zipped fst files to work on later

--- egs/cfg2/tutorial-2/scripts/gen-allwords.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `cat data/wordlist`; do
-  scripts/gen-word.pl $i | fstcompile --isymbols=data/letter.voc --osymbols=data/word.voc > dict/$i.fst
-done
--- egs/cfg2/tutorial-2/scripts/gen-sent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scripts/gen-word.pl `sed -e 's/  */\#/g' -e 's/ *$/\#/' $1` | \
-  gawk 'NF==4 {$4=""} {print}'
-
