01459b7b4e84e542e01b147a75f40e3e510a26e2
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-09 14:40:11 +0100

    added build docker image script

---
+++ ci/build-docker-image.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Docker image builder
+# This script builds the docker image and pushes it to the Docker registry with
+# image tag as the branch name of the repository
+
+# Stop on errors
+set -o errexit
+
+# Execute script only in a CI environment
+if [ "$CI" != "true" ]
+then
+    echo -e "\e[35m\e[1m Error!\e[0m Trying to execute a CI script in a non-CI environment. Exiting script."
+    exit 1
+fi
+
+default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+echo "Repository default branch: $default_branch"
+
+if [ "$default_branch" == "$TRAVIS_BRANCH" ]
+then
+    IMAGE_TAG="latest"
+else
+    IMAGE_TAG=$(echo "$TRAVIS_BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g')
+fi
+
+# Create tag based on branch name
+export IMAGE_NAME=tueroboticsamigo/speech_recognition:"$IMAGE_TAG"
+
+echo -e "\e[35m\e[1m Creating docker $IMAGE_NAME \e[0m"
+
+# Build the Docker image (this will use the Dockerfile in the root of the repo)
+docker build --build-arg BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} --build-arg \
+COMMIT="$TRAVIS_COMMIT" --build-arg PULL_REQUEST="$TRAVIS_PULL_REQUEST" -t $IMAGE_NAME .
+
