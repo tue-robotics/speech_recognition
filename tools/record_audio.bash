c82fc47baa324e6ac78ff5dda9fe519961e4490d
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-26 20:21:05 +0100

    update

--- tools/record_audio.bash
+++ tools/record_audio.bash
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-gst-launch-1.0 -e pulsesrc ! audioconvert ! wavenc ! filesink location=test.wav
+gst-launch-1.0 -e pulsesrc ! audioconvert ! \
+    audio/x-raw, rate=16000, channels=1 ! \
+    wavenc ! filesink location=test.wav
