099b443b36ccfe3dd5909033109ab5417dae58bf
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-26 20:55:12 +0100

    added print statements

--- tools/record_audio.bash
+++ tools/record_audio.bash
@@ -1,5 +1,14 @@
 #! /usr/bin/env bash
 
+input_src=$(pactl list | grep -A2 'Source #' | grep 'Name: ' | cut -d" " -f2)
+
+echo -e "Audio sources found:"
+echo -e "--------------------"
+echo -e "$input_src"
+echo
+echo -e "Setting up gstreamer pipeline:"
+echo -e "------------------------------"
+
 if [ -z "$1" ]
 then
     gst-launch-1.0 -e pulsesrc ! audioconvert ! \
@@ -10,3 +19,7 @@ else
     audio/x-raw, rate=16000, channels=1 ! \
     wavenc ! filesink location=test.wav
 fi
+
+echo
+echo -e "Recording complete"
+echo -e "------------------------------"
