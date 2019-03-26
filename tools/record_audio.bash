71ad124a086bc39e5d4f014423fdcc9f8e7e47e9
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-26 20:49:18 +0100

    added input src option

--- tools/record_audio.bash
+++ tools/record_audio.bash
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
-gst-launch-1.0 -e pulsesrc ! audioconvert ! \
+
+if [ -z "$1" ]
+then
+    gst-launch-1.0 -e pulsesrc ! audioconvert ! \
     audio/x-raw, rate=16000, channels=1 ! \
     wavenc ! filesink location=test.wav
+else
+    gst-launch-1.0 -e pulsesrc device="$1" ! audioconvert ! \
+    audio/x-raw, rate=16000, channels=1 ! \
+    wavenc ! filesink location=test.wav
+fi
