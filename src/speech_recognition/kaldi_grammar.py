33125c55a2fa340cd0117e497b20955077d48477
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-06 15:20:15 +0200

    fixed corpus creation

--- src/speech_recognition/kaldi_grammar.py
+++ src/speech_recognition/kaldi_grammar.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
+
+# Make python 2/3 compatible
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
 import os
+import shutil
 from grammar_parser.cfgparser import CFGParser
 
 
@@ -9,7 +16,7 @@ class KaldiGrammar:
     get_rule_element: extracts the defined grammar rules
     get_words: extracts the unique words and creates 'corpus.txt' which is used to build 'G.fst'
     """
-    def __init__(self, grammar_file_string, target):
+    def __init__(self, grammar_file_string, target, model_path):
 
         if os.path.exists(grammar_file_string):
             self.parser = CFGParser.fromfile(grammar_file_string)
@@ -19,8 +26,16 @@ class KaldiGrammar:
             self.grammar_string = grammar_file_string
 
         self.target = target
+        self.model_path = model_path
+        self.model_path_tmp = os.path.join(self.model_path, "tmp")
 
-    # ----------------------------------------------------------------------------------------------------
+        if not os.path.exists(self.model_path):
+            raise Exception("Model path '{}' does not exist".format(self.model_path))
+        else:
+            if os.path.exists(self.model_path_tmp):
+                shutil.rmtree(self.model_path_tmp)
+
+            os.mkdir(self.model_path_tmp)
 
 
     def get_words(self):
@@ -46,10 +61,12 @@ class KaldiGrammar:
                     if not conjunct.is_variable:
                         words.add(conjunct.name)
 
-        words = [word.upper() + "\n" for word in list(words)]
+        words = [word.upper() for word in list(words)]
+        words.sort()
 
         # Create corpus.txt file and save the words list
-        with open("corpus.txt", "w") as f:
+        corpus_path = os.path.join(self.model_path_tmp, "corpus.txt")
+        with open(corpus_path, "w") as f:
             for word in words:
-                f.write(word)
+                f.write(word + "\n")
 
