69d31a5eb00616c72f16b6fee518877ee0bf3ea4
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-07 12:48:19 +0200

    make tri2b_mmi_boost model

--- egs/voxforge/run.sh
+++ egs/voxforge/run.sh
@@ -3,6 +3,13 @@
 # Copyright 2012 Vassil Panayotov
 # Apache 2.0
 
+# NOTE: You will want to download the data set first, before executing this script.
+#       This can be done for example by:
+#       1. Setting the variable DATA_ROOT in path.sh to point to a
+#          directory with enough free space (at least 20-25GB
+#          currently (Feb 2014))
+#       2. Running "getdata.sh"
+
 # The second part of this script comes mostly from egs/rm/s5/run.sh
 # with some parameters changed
 
@@ -18,7 +25,7 @@ njobs=8
 
 # This recipe can select subsets of VoxForge's data based on the "Pronunciation dialect"
 # field in VF's etc/README files. To select all dialects, set this to "English"
-dialects="((American)|(British)|(Australia)|(Zealand))"
+dialects="English"
 
 # The number of randomly selected speakers to be put in the test set
 nspk_test=20
@@ -39,9 +46,45 @@ selected=${DATA_ROOT}/selected
 
 [[ $# -ge 1 ]] && { echo "Unexpected arguments"; exit 1; }
 
+echo "Start time:"
+echo "-----------"
+timedatectl
+
+echo
+echo -e "\e[35m\e[1m==== Data Preparation ====\e[0m"
+echo
+
+# Select a subset of the data to use
+# WARNING: the destination directory will be deleted if it already exists!
+local/voxforge_select.sh --dialect $dialects \
+  ${DATA_ROOT}/extracted ${selected} || exit 1
+
+# Mapping the anonymous speakers to unique IDs
+local/voxforge_map_anonymous.sh ${selected} || exit 1
+
+# Initial normalization of the data
+local/voxforge_data_prep.sh --nspk_test ${nspk_test} ${selected} || exit 1
+
+# Prepare ARPA LM and vocabulary using SRILM
+local/voxforge_prepare_lm.sh --order ${lm_order} || exit 1
+
+# Prepare the lexicon and various phone lists
+# Pronunciations for OOV words are obtained using a pre-trained Sequitur model
+local/voxforge_prepare_dict.sh || exit 1
+
+# Prepare data/lang and data/local/lang directories
+utils/prepare_lang.sh --position-dependent-phones $pos_dep_phones \
+  data/local/dict '!SIL' data/local/lang data/lang || exit 1
+
+# Prepare G.fst and data/{train,test} directories
+local/voxforge_format_data.sh || exit 1
+
 # Now make MFCC features.
 # mfccdir should be some place with a largish disk where you
 # want to store MFCC features.
+echo
+echo -e "\e[35m\e[1m==== Feature Extraction ====\e[0m"
+echo
 mfccdir=${DATA_ROOT}/mfcc
 for x in train test; do
  steps/make_mfcc.sh --cmd "$train_cmd" --nj $njobs \
@@ -50,6 +93,9 @@ for x in train test; do
 done
 
 # Train monophone models on a subset of the data
+echo
+echo -e "\e[35m\e[1m==== mono ====\e[0m"
+echo
 utils/subset_data_dir.sh data/train 1000 data/train.1k  || exit 1;
 steps/train_mono.sh --nj $njobs --cmd "$train_cmd" data/train.1k data/lang exp/mono  || exit 1;
 
@@ -66,6 +112,9 @@ steps/align_si.sh --nj $njobs --cmd "$train_cmd" \
   data/train data/lang exp/mono exp/mono_ali || exit 1;
 
 # train tri1 [first triphone pass]
+echo
+echo -e "\e[35m\e[1m==== tri1 ====\e[0m"
+echo
 steps/train_deltas.sh --cmd "$train_cmd" \
   2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1;
 
@@ -81,6 +130,9 @@ steps/align_si.sh --nj $njobs --cmd "$train_cmd" \
   --use-graphs true data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
 
 # train tri2a [delta+delta-deltas]
+echo
+echo -e "\e[35m\e[1m==== tri2a ====\e[0m"
+echo
 steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 \
   data/train data/lang exp/tri1_ali exp/tri2a || exit 1;
 
@@ -88,6 +140,43 @@ steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 \
 utils/mkgraph.sh data/lang_test exp/tri2a exp/tri2a/graph
 steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
   exp/tri2a/graph data/test exp/tri2a/decode
+
+# train and decode tri2b [LDA+MLLT]
+echo
+echo -e "\e[35m\e[1m==== tri2b ====\e[0m"
+echo
+steps/train_lda_mllt.sh --cmd "$train_cmd" 2000 11000 \
+  data/train data/lang exp/tri1_ali exp/tri2b || exit 1;
+utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph
+steps/decode.sh --config conf/decode.config --nj $njobs --cmd "$decode_cmd" \
+  exp/tri2b/graph data/test exp/tri2b/decode
+
+# Align all data with LDA+MLLT system (tri2b)
+steps/align_si.sh --nj $njobs --cmd "$train_cmd" --use-graphs true \
+   data/train data/lang exp/tri2b exp/tri2b_ali || exit 1;
+
+#  Do MMI on top of LDA+MLLT.
+echo
+echo -e "\e[35m\e[1m==== tri2b_mmi ====\e[0m"
+echo
+steps/make_denlats.sh --nj $njobs --cmd "$train_cmd" \
+  data/train data/lang exp/tri2b exp/tri2b_denlats || exit 1;
+steps/train_mmi.sh data/train data/lang exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mmi || exit 1;
+steps/decode.sh --config conf/decode.config --iter 4 --nj $njobs --cmd "$decode_cmd" \
+   exp/tri2b/graph data/test exp/tri2b_mmi/decode_it4
+steps/decode.sh --config conf/decode.config --iter 3 --nj $njobs --cmd "$decode_cmd" \
+   exp/tri2b/graph data/test exp/tri2b_mmi/decode_it3
+
+# Do the same with boosting.
+echo
+echo -e "\e[35m\e[1m==== tri2b_mmi with boosting ====\e[0m"
+echo
+steps/train_mmi.sh --boost 0.05 data/train data/lang \
+   exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mmi_b0.05 || exit 1;
+steps/decode.sh --config conf/decode.config --iter 4 --nj $njobs --cmd "$decode_cmd" \
+   exp/tri2b/graph data/test exp/tri2b_mmi_b0.05/decode_it4 || exit 1;
+steps/decode.sh --config conf/decode.config --iter 3 --nj $njobs --cmd "$decode_cmd" \
+   exp/tri2b/graph data/test exp/tri2b_mmi_b0.05/decode_it3 || exit 1;
 
 echo
 echo -e "\e[35m\e[1m==== Calculate Best Word Error Rate (WER) ====\e[0m"
@@ -102,5 +191,7 @@ echo
 echo -e "\e[35m\e[1m==== Execution completed ====\e[0m"
 echo
 
-
+echo "End time:"
+echo "-----------"
+timedatectl
 
