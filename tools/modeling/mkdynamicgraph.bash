825e0769cd4c966c25c6b83d0e876fe488591432
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-07 15:35:10 +0200

    added dynamic grammar creation in the graph script

---
+++ tools/modeling/mkdynamicgraph.bash
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+if [ -z "$1" ]
+then
+    echo "Model path not specified"
+    exit 1
+fi
+
+model_path="$1"
+
+if [ -z "$2" ]
+then
+    echo "Path to 'corpus.txt' not specified"
+    exit 1
+fi
+model_path_tmp="$2"
+
+if [ ! -f "$model_path_tmp/corpus.txt" ]
+then
+    echo "corpus.txt does not exist at '$model_path_tmp/corpus.txt'"
+    exit 1
+fi
+
+mkdynamicgrammar.bash "$model_path" "$model_path_tmp"
