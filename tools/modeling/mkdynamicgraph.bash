09380f0c645e51f0a1d4038be9a89b0ce7b6927e
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-07 16:15:47 +0200

    added complete graph creation

--- tools/modeling/mkdynamicgraph.bash
+++ tools/modeling/mkdynamicgraph.bash
@@ -15,10 +15,14 @@ then
 fi
 model_path_tmp="$2"
 
-if [ ! -f "$model_path_tmp/corpus.txt" ]
+if [ ! -f "$model_path_tmp"/corpus.txt ]
 then
     echo "corpus.txt does not exist at '$model_path_tmp/corpus.txt'"
     exit 1
 fi
 
 mkdynamicgrammar.bash "$model_path" "$model_path_tmp"
+
+mkgraph.sh "$model_path_tmp" "$model_path" "$model_path_tmp"/graph
+
+cp "$model_path_tmp"/graph/HCLG.fst "$model_path"
