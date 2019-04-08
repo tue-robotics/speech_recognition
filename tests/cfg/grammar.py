a10a31260d56f3df31ca9cef4dc9f35fab4fa138
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-08 09:11:24 +0200

    updated test file
    includes test for both tree and dynamic grammar creation

--- tests/cfg/grammar.py
+++ tests/cfg/grammar.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
-from speech_recognition.kaldi_grammar import Grammar, print_graphviz
+# Make python 2/3 compatible
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+from speech_recognition.kaldi_grammar import Grammar
 
 if __name__ == "__main__":
     import sys
@@ -17,24 +22,20 @@ if __name__ == "__main__":
     dummy_model_path = os.path.dirname(os.path.realpath(__file__))
 
     k = Grammar(dummy_model_path, grammar_file, target)
-#    k.print_graphviz()
+    k.print_graphviz()
 
-#    print()
     subprocess_exit_status = subprocess.call(["mkdynamicgrammar.bash",
         k.model_path, k.model_path_tmp])
 
     if subprocess_exit_status == 1:
         raise Exception("Subprocess error")
 
+    test_sentence_1 = "bring me the coke"
+    test_sentence_2 = "bring the coke to the kitchen"
 
+    s1 = k.parse(test_sentence_1)
+    s2 = k.parse(test_sentence_2)
 
-
-#    test_sentence_1 = "bring me the coke"
-#    test_sentence_2 = "bring the coke to the kitchen"
-
-#    s1 = k.parse(test_sentence_1)
-#    s2 = k.parse(test_sentence_2)
-
-#    print(s1)
-#    print(s2)
+    print("Parser output for '{}': ".format(test_sentence_1), s1)
+    print("Parser output for '{}': ".format(test_sentence_2), s2)
 
