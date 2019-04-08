541a1bc63fc0a399fb038d792774c5bd29c5564a
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-08 10:15:50 +0200

    fixed path to modeling tools

--- env-hooks/setup.bash
+++ env-hooks/setup.bash
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/tools/modeling${PATH:+:${PATH}}
+PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../tools/modeling${PATH:+:${PATH}}
 export PATH
